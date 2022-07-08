ae2e600f3e331a5340064bd9883105994cc715ff
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-07-08 12:02:38 +0530

    datatypes Default Values

---
+++ DefaultValue.java
@@ -0,0 +1,22 @@
+// default value program (only static and instance)
+
+class DefaultValue{
+	static public void main(String... args){
+		System.out.println(price);
+		System.out.println(rate);
+		System.out.println(bite);
+		System.out.println(shrt);
+		System.out.println(bl);
+		System.out.println(character);// default value is empty space..
+		System.out.println(value);
+		System.out.println(salary);
+	}
+	static int	price;
+	static long rate;
+	static byte bite;
+	static short shrt;
+	static boolean bl; 
+	static char character;
+	static float value;
+	static double salary;
+}
